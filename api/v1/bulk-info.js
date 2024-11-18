163adf6d31f4fef13b3cde7218ba7119f23cea75
William Baewer <300087423@fvtc.edu>
2024-11-18 13:58:35 -0600

    task failed succesfully

---
+++ api/v1/bulk-info.js
@@ -0,0 +1,9 @@
+const info = { "summary" : [
+    {"ID":"id"},
+    {"Title": "title"},
+    {"Image": "image"},
+    {"PrepTime": "prep time"},
+    {"Difficulty": "Difficulty"}
+] }
+
+module.exports = info
